8c280c7bafbb61e001376af5597637bbf77602cc
dhasegan <danielhasegan@gmail.com>
2013-11-13 18:23:18 -0500

    Changed the layout and design

--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
 # Create your views here.
 from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+import random
 
 def home(request):
-    if request.method != 'POST':
-        return render(request, 'base.html', {})
-    return render(request, 'base.html', {})
+    context = {}
+
+    if request.method == 'POST' and 'text' in request.POST and request.POST['text']:
+        context['answered'] = True
+
+        lines = []
+        f = open('thirdworldproblems.txt', 'r')
+        for l in f:
+            lines.append( l );
+        f.close()
+        nrlines = len(lines)
+        context['text'] = lines[ random.randint(0,nrlines-1) ]
+
+        return render(request, 'base.html', context)
+
+    return render(request, 'base.html', context)
